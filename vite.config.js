8085ac4bf1e88c159398d19d09cbf510d1eeffae
andreideak <48556094+andreideak@users.noreply.github.com>
2022-12-27 16:01:42 +0100

    changed port to 7301

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	server: {
+		port: 7301,
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
